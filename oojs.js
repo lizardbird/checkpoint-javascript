cb400ff999f5f4866508e3902273e680c7cb26f3
Liz Jewell <eghjewell@gmail.com>
2017-07-10 09:30:01 -0400

    update oojs.js

--- oojs.js
+++ oojs.js
@@ -5,16 +5,24 @@
 // - a `songs` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addSong` method that adds a song (string) to the `songs` array
 
+class Playlist {
+  constructor (title) {
+    this.title = title
+    this.songs = []
+  }
 
+  addSong (song) {
+    this.songs.push(song)
+  }
+}
 
 // Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
-
-
-
+var myPlaylist = new Playlist('My Playlist')
+myPlaylist.addSong('Sittin On The Dock of the Bay')
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
-if(typeof Playlist !== 'undefined') {
+if (typeof Playlist !== 'undefined') {
   module.exports = {
     Playlist
   }
